2314157a24d809075641783f7f161aea8fe52baa
hugo <hugo.cruz@mercadolivre.com>
2022-03-18 11:26:09 -0300

    classe corrida

---
+++ projeto-java/src/exercicios/dakar/Corrida.java
@@ -0,0 +1,10 @@
+package exercicios.dakar;
+
+import java.math.BigDecimal;
+
+public class Corrida {
+    private int distancia;
+    private BigDecimal premioUSD;
+    private String Nome;
+    private int numMaxVeiculos;
+}
