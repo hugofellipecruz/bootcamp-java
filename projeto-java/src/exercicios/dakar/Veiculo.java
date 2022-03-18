afa2d71e3f681457eba26763bd482c5c5b7972c5
hugo <hugo.cruz@mercadolivre.com>
2022-03-18 11:24:42 -0300

    classe veiculo

---
+++ projeto-java/src/exercicios/dakar/Veiculo.java
@@ -0,0 +1,11 @@
+package exercicios.dakar;
+
+public class Veiculo {
+    private int velocidade;
+    private int aceleração;
+    private int angulo;
+    private String placa;
+    private int peso;
+    private int rodas;
+
+}
